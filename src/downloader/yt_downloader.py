a6d627cfb55967278400543d6ff544f5cc2671df
Thiago Baratieri <thiagobaratieri971@gmail.com>
2025-01-07 02:42:27 -0300

    Teste para verificação da logica usada - Tudo certo

--- src/downloader/yt_downloader.py
+++ src/downloader/yt_downloader.py
@@ -1,4 +1,5 @@
 import re
+import yt_dlp
 
 
 def download_audio(youtube_url, output_path="downloads/"):
@@ -14,17 +15,26 @@ def download_audio(youtube_url, output_path="downloads/"):
     """
     
     # 1. Validar o link do YouTube
-    padrao = r"^(https:\/\/(www\.)?youtube\.com\/watch\?v=[A-Za-z0-9_-]+|https:\/\/youtu\.be\/[A-Za-z0-9_-]+)$"
+    padrao = r"^(https:\/\/(www\.)?youtube\.com\/watch\?v=[A-Za-z0-9_-]+|https:\/\/youtu\.be\/[A-Za-z0-9_-]+)$" # REGEX
     if not re.match(padrao, youtube_url):
         return "URL Inválida ou Inexistente. Forneça um link válido do youtube"
 
     
     # 2. Obter o vídeo com o pytube
-    
-    # 3. Selecionar apenas o áudio
-    
-    # 4. Salvar no diretório especificado
+    ydl_opts = {
+        'format': 'bestaudio/best',  # Melhor qualidade de áudio disponível
+        'extractaudio': True,  # Extrair apenas o áudio
+        'audioquality': 1,  # Melhor qualidade
+        'outtmpl': f'{output_path}%(title)s.%(ext)s',  # Caminho e nome do arquivo
+    }
 
-    return "Link válido, pronto para o download"
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([youtube_url])
+        return f"Audio baixado com sucesso! Arquivo salvo em: {output_path}"
+    except Exception as e:
+        return f"Erro ao baixar audio: {e}"
 
-print(download_audio("https://www.youtube.com/watch?v=BzFxuDIEcUI"))
+
+
+print(download_audio("https://www.youtube.com/watch?v=BM-mnklMWCQ"))
